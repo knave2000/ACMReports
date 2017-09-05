afd3eda9b74818475894117acb53d2ee0f0da876
knave2000 <knave2000@gmail.com>
2017-09-05 19:15:51 +0300

    UTC time for SQL request

--- ACMReports/Form1.Designer.cs
+++ ACMReports/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ACMReports
 {
-    partial class Form1
+    partial class WorktimeForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 590);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Configure Connection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(59, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Date:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(692, 590);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Export to PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(879, 520);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(464, 590);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Read Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(237, 590);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Edit Request";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // WorktimeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(904, 629);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "WorktimeForm";
+            this.Text = "Worktime control";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ACMReports/Form1.cs
+++ ACMReports/Form1.cs
@@ -1,20 +1,279 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using Npgsql;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+
+// Creation date: 01.09.2017
+// Author: Vladimir Artyukhov, knave2000@gmail.com
 
 namespace ACMReports
 {
-    public partial class Form1 : Form
+    public partial class WorktimeForm : Form
     {
-        public Form1()
+        public WorktimeForm()
         {
             InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        private int rowNum = new int();             // total rows in DataTable
+        private const int rowsPerPage = 30;         // the number of rows in table per page
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created by ACM Report Generator.";
+            document.Info.Subject = "Worktime report created by ACM Report Generator.";
+            document.Info.Author = "ACM Report";
+            document.Info.Keywords = "ACM Report, Worktime";
+
+            // Define the total number of pages in document
+            int pageLast = rowNum / rowsPerPage + 1;  // the last page number
+
+            // Make the page(s)
+            for (int pageNum = 0; pageNum < pageLast; pageNum++)
+            {
+                Page1(document, pageNum + 1);
+            }
+
+            // Make directory to store reports
+            const string directory = "Reports";
+            Directory.CreateDirectory(directory);
+
+            // Save the document
+            string filename = "ACMReports_" + dateTimePicker1.Value.Date.ToString("yyyyMMdd") + ".pdf";
+            document.Save(directory + Path.DirectorySeparatorChar + filename);
+
+            // Start PDF viewer
+            Process.Start(directory + Path.DirectorySeparatorChar + filename);
+        }
+
+        private void Page1(PdfDocument document, int pageNum)
+        {
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            // Unicode hack
+            gfx.MUH = PdfFontEncoding.Unicode;
+
+            // Get the A4 page size
+            Unit width, height;
+            PageSetup.GetPageSize(PageFormat.A4, out width, out height);
+
+            XFont font = new XFont("Verdana", 13, XFontStyle.Bold);
+
+            // You always need a MigraDoc document for rendering.
+            Document doc = new Document();
+
+            // Add a section to the document and configure it such
+            // that it will be in the center of the page
+            Section section = doc.AddSection();
+            section.PageSetup.PageHeight = height;
+            section.PageSetup.PageWidth = width;
+            section.PageSetup.LeftMargin = 0;
+            section.PageSetup.RightMargin = 0;
+            section.PageSetup.TopMargin = 20;
+
+            // Add a top paragraph with date
+            Paragraph para = section.AddParagraph();
+            para.Format.Alignment = ParagraphAlignment.Right;
+            para.Format.Font.Name = "Times New Roman";
+            para.Format.Font.Size = 12;
+            para.Format.Font.Color = MigraDoc.DocumentObjectModel.Colors.DarkGray;
+            para.AddFormattedText(dateTimePicker1.Value.Date.ToString("dddd, d MMMM yyyy"), TextFormat.Bold);
+
+            // Add a bottom paragraph with page number
+            Paragraph pg = section.AddParagraph();
+            pg.Format.Alignment = ParagraphAlignment.Right;
+            pg.Format.Font.Name = "Times New Roman";
+            pg.Format.Font.Size = 12;
+            pg.Format.Font.Color = MigraDoc.DocumentObjectModel.Colors.DarkGray;
+            pg.AddFormattedText("Page " + pageNum, TextFormat.Bold);
+
+            // Create a table
+            Table table = new Table();
+            table.Borders.Visible = true;
+            table.Borders.Width = 1; // Default to show borders 1 pixel wide Column
+            table.TopPadding = 5;
+            table.BottomPadding = 5;
+
+            Column column = table.AddColumn(40);
+            column.Format.Alignment = ParagraphAlignment.Left;
+
+            column = table.AddColumn(150);
+            column.Format.Alignment = ParagraphAlignment.Left;
+
+            column = table.AddColumn(120);
+            column.Format.Alignment = ParagraphAlignment.Center;
+
+            column = table.AddColumn(120);
+            column.Format.Alignment = ParagraphAlignment.Center;
+
+            column = table.AddColumn(80);
+            column.Format.Alignment = ParagraphAlignment.Center;
+
+            table.Rows.Height = 20;
+
+            Row row = table.AddRow();
+            row.Shading.Color = Colors.PaleGoldenrod;
+            row.VerticalAlignment = VerticalAlignment.Top;
+
+            // Table header
+            row.Cells[0].AddParagraph("#");
+            row.Cells[1].AddParagraph("Name");
+            row.Cells[2].AddParagraph("Start");
+            row.Cells[3].AddParagraph("End");
+            row.Cells[4].AddParagraph("Duration");
+
+            // Define the last row number on the current page
+            int lastNum;
+            if (rowNum >= pageNum * rowsPerPage)
+            {
+                lastNum = pageNum * rowsPerPage;
+            }
+            else
+            {
+                lastNum = rowNum;
+            }
+
+            // Put the rows into the table
+            DataRow dr = dt.Rows[0];
+            for (int i = 1 + (pageNum - 1) * rowsPerPage; i <= lastNum; i++)
+            {
+                row = table.AddRow();
+                row.Cells[0].AddParagraph(i.ToString());
+                row.Cells[1].AddParagraph(dr["name"].ToString());
+                row.Cells[2].AddParagraph(dr["start"].ToString());
+                row.Cells[3].AddParagraph(dr["end"].ToString());
+                row.Cells[4].AddParagraph(dr["duration"].ToString());
+            }
+
+            table.SetEdge(0, 0, 5, 1, Edge.Box, MigraDoc.DocumentObjectModel.BorderStyle.Single, 1.5, Colors.Black);
+
+            doc.LastSection.Add(table);
+
+            // Create a renderer and prepare (=layout) the document
+            MigraDoc.Rendering.DocumentRenderer docRenderer = new DocumentRenderer(doc);
+            docRenderer.PrepareDocument();
+
+            // Render the paragraph. You can render tables or shapes the same way.
+            docRenderer.RenderObject(gfx, XUnit.FromCentimeter(10), XUnit.FromCentimeter(1), "10cm", para);
+            docRenderer.RenderObject(gfx, XUnit.FromCentimeter(2), XUnit.FromCentimeter(2), "12cm", table);
+            docRenderer.RenderObject(gfx, XUnit.FromCentimeter(10), XUnit.FromCentimeter(28), "10cm", pg);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 myForm2 = new Form2();
+            myForm2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Read settings from application configuration file
+                string ACMServerIP = Properties.Settings.Default["ACMServerIP"].ToString();
+                string ACMServerPort = Properties.Settings.Default["ACMServerPort"].ToString();
+                string ACMUsername = Properties.Settings.Default["ACMUsername"].ToString();
+                string ACMPassword = Properties.Settings.Default["ACMPassword"].ToString();
+                string EntranceEvent = Properties.Settings.Default["EntranceEvent"].ToString();
+                string EntranceDoor = Properties.Settings.Default["EntranceDoor"].ToString();
+                string ExitEvent = Properties.Settings.Default["ExitEvent"].ToString();
+                string ExitDoor = Properties.Settings.Default["ExitDoor"].ToString();
+
+                // PostgeSQL-style connection string
+                // SSL encryption is required for ACM connection
+                string connStr = String.Format("Server={0};Port={1};UserId={2};Password={3};" +
+                        "Database=TransactionDB;SSL Mode=Require;Trust Server Certificate=true",
+                        ACMServerIP, ACMServerPort, ACMUsername, ACMPassword);
+
+                NpgsqlConnection conn = new NpgsqlConnection(connStr);
+                conn.Open();
+
+                // Prepare SQL Request
+                string startDate = dateTimePicker1.Value.Date.ToString("yyyy-MM-dd");
+                string endDate = dateTimePicker1.Value.AddDays(1).Date.ToString("yyyy-MM-dd");
+
+                string SQLRequest = String.Format(
+                "SELECT " +
+                "    row_number() OVER (ORDER BY s.fn1 NULLS LAST) AS number, " +
+                "    s.fn1 AS name, " +
+                "    s.start_date AS start, " +
+                "    e.end_date AS end, " +
+                "    e.end_date - s.start_date AS duration " +
+                "FROM " +
+                "( " +
+                "    SELECT " +
+                "        to_timestamp(min(trxdateutc)) AS start_date, " +
+                "        fullname AS fn1 " +
+                "    FROM rpt_alltrx " +
+                "    WHERE eventname like '%{0}' " +
+                "        AND sourcename like '%{1}%' " +
+                "        AND to_timestamp(trxdateutc) >= to_timestamp('{4}', 'YYYY-MM-DD') " +
+                "        AND to_timestamp(trxdateutc) <= to_timestamp('{5}', 'YYYY-MM-DD') " +
+                "    GROUP BY fullname " +
+                ") AS s " +
+                "LEFT JOIN " +
+                "( " +
+                "    SELECT " +
+                "        to_timestamp(max(trxdateutc)) AS end_date, " +
+                "        fullname AS fn2" +
+                "    FROM rpt_alltrx " +
+                "    WHERE eventname like '%{2}%' " +
+                "        AND sourcename like '%{3}%' " +
+                "        AND to_timestamp(trxdateutc) >= to_timestamp('{4}', 'YYYY-MM-DD') " +
+                "        AND to_timestamp(trxdateutc) <= to_timestamp('{5}', 'YYYY-MM-DD') " +
+                "    GROUP BY fullname " +
+                ") AS e " +
+                "ON s.fn1 = e.fn2 " +
+                "ORDER BY name ASC;",
+                EntranceEvent, EntranceDoor, ExitEvent, ExitDoor, startDate, endDate);
+
+                // Execute SQL Request
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(SQLRequest, conn);
+
+                // Display the result in GridView
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+                conn.Close();
+
+                // Enable Export button
+                rowNum = dt.Rows.Count;
+                if (rowNum > 0)
+                {
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    button2.Enabled = false;
+                }
+            }
+            catch
+            {
+                // if something went wrong, and we want to know why
+                MessageBox.Show("Connection or internal error.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Open SQL Request form
+            Form3 myForm3 = new Form3();
+            myForm3.Show();
         }
     }
 }
